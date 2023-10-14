a9fb6a4feb772bed6b642df33f885b4ce08b572e
up202105327 <up202105327@fe.up.pt>
2023-10-14 18:29:05 +0100

    BigBang

---
+++ database/sql/schema.sql
@@ -0,0 +1,146 @@
+-----------------------------------------
+-- Drop old schema
+-----------------------------------------
+
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Admin CASCADE;
+DROP TABLE IF EXISTS Authenticated CASCADE;
+DROP TABLE IF EXISTS Event CASCADE;
+DROP TABLE IF EXISTS EventMessage CASCADE;
+DROP TABLE IF EXISTS MessageReaction CASCADE;
+DROP TABLE IF EXISTS UserNotifications CASCADE;
+DROP TABLE IF EXISTS Notification CASCADE;
+DROP TABLE IF EXISTS EventParticipants CASCADE;
+DROP TABLE IF EXISTS FavoriteEvent CASCADE;
+DROP TABLE IF EXISTS Hashtag CASCADE;
+DROP TABLE IF EXISTS Ticket CASCADE;
+DROP TABLE IF EXISTS Report CASCADE;
+DROP TABLE IF EXISTS EventReport CASCADE;
+DROP TABLE IF EXISTS UploadFile CASCADE;
+
+-----------------------------------------
+-- Create tables
+-----------------------------------------
+
+CREATE TABLE Users (
+    id INT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Admin (
+    id_user INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES Users(id)
+);
+
+CREATE TABLE Authenticated (
+    id_user INT PRIMARY KEY,
+    is_verified BOOLEAN DEFAULT FALSE,
+    id_profilepic INT DEFAULT '0',
+    FOREIGN KEY (id_user) REFERENCES Users(id)
+);
+
+CREATE TABLE Event (
+    id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    type VARCHAR(255) CHECK (type IN ('TypesEvent')),
+    date DATE NOT NULL,
+    capacity INT NOT NULL,
+    place VARCHAR(255) NOT NULL,
+    id_user INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES Authenticated(id_user)
+);
+
+CREATE TABLE EventMessage (
+    id INT PRIMARY KEY,
+    type VARCHAR(255) CHECK (type IN ('TypesMessage')),
+    content TEXT NOT NULL,
+    id_event INT NOT NULL,
+    id_user INT NOT NULL,
+    date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE MessageReaction (
+    id_user INT NOT NULL,
+    id_message INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES Authenticated(id_user),
+    FOREIGN KEY (id_message) REFERENCES EventMessage(id)
+);
+
+CREATE TABLE Notification (
+    id INT PRIMARY KEY,
+    type VARCHAR(255) CHECK (type IN ('TypesNotification')),
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    date DATE DEFAULT CURRENT_DATE,
+    id_event INT NOT NULL,
+    FOREIGN KEY (id_event) REFERENCES Event(id)
+);
+
+CREATE TABLE UserNotifications (
+    id_user INT NOT NULL,
+    id_notification INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES Users(id),
+    FOREIGN KEY (id_notification) REFERENCES Notification(id)
+);
+
+CREATE TABLE EventParticipants (
+    id_user INT NOT NULL,
+    id_event INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES Authenticated(id_user),
+    FOREIGN KEY (id_event) REFERENCES Event(id)
+);
+
+CREATE TABLE FavoriteEvent (
+    id_user INT NOT NULL,
+    id_event INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES Authenticated(id_user),
+    FOREIGN KEY (id_event) REFERENCES Event(id)
+);
+
+CREATE TABLE Hashtag (
+    id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    id_event INT NOT NULL,
+    FOREIGN KEY (id_event) REFERENCES Event(id)
+);
+
+CREATE TABLE Ticket (
+    id INT PRIMARY KEY,
+    id_event INT NOT NULL,
+    id_user INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    description TEXT,
+    title VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id_event) REFERENCES Event(id),
+    FOREIGN KEY (id_user) REFERENCES Authenticated(id_user)
+);
+
+CREATE TABLE Report (
+    id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE EventReport (
+    id_event INT NOT NULL,
+    id_report INT NOT NULL,
+    FOREIGN KEY (id_event) REFERENCES Event(id),
+    FOREIGN KEY (id_report) REFERENCES Report(id)
+);
+
+CREATE TABLE UploadFile (
+    id INT PRIMARY KEY,
+    path VARCHAR(255) NOT NULL,
+    id_user INT NOT NULL,
+    id_message INT,
+    id_report INT,
+    id_event INT,
+    FOREIGN KEY (id_user) REFERENCES Authenticated(id_user),
+    FOREIGN KEY (id_message) REFERENCES EventMessage(id),
+    FOREIGN KEY (id_report) REFERENCES Report(id),
+    FOREIGN KEY (id_event) REFERENCES Event(id)
+);
