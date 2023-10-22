65e27877eb2f0b49cdfcb9be8cb46dd63f95d1ac
up202105327 <up202105327@fe.up.pt>
2023-10-22 13:19:14 +0100

    Finished schema SQL

--- database/sql/schema.sql
+++ database/sql/schema.sql
@@ -1,62 +1,38 @@
------------------------------------------
 -- Drop old schema
------------------------------------------
 
-DROP TABLE IF EXISTS Users CASCADE;
-DROP TABLE IF EXISTS Admin CASCADE;
-DROP TABLE IF EXISTS Authenticated CASCADE;
-DROP TABLE IF EXISTS Event CASCADE;
-DROP TABLE IF EXISTS EventMessage CASCADE;
-DROP TABLE IF EXISTS MessageReaction CASCADE;
-DROP TABLE IF EXISTS UserNotifications CASCADE;
-DROP TABLE IF EXISTS Notification CASCADE;
 DROP TABLE IF EXISTS EventParticipants CASCADE;
 DROP TABLE IF EXISTS FavoriteEvent CASCADE;
-DROP TABLE IF EXISTS Hashtag CASCADE;
-DROP TABLE IF EXISTS Ticket CASCADE;
-DROP TABLE IF EXISTS Report CASCADE;
-DROP TABLE IF EXISTS EventReport CASCADE;
-DROP TABLE IF EXISTS UploadFile CASCADE;
-DROP TABLE IF EXISTS Poll CASCADE;
-DROP TABLE IF EXISTS PollOptions CASCADE;
+DROP TABLE IF EXISTS EventHashtag CASCADE;
 DROP TABLE IF EXISTS PollVotes CASCADE;
-
------------------------------------------
--- Drop old types
------------------------------------------
-
-DROP TYPE IF EXISTS TypesEvent CASCADE;
-DROP TYPE IF EXISTS TypesMessage CASCADE;
+DROP TABLE IF EXISTS UserNotifications CASCADE;
+DROP TABLE IF EXISTS Notification CASCADE;
+DROP TABLE IF EXISTS MessageReaction CASCADE;
+DROP TABLE IF EXISTS EventMessage CASCADE;
+DROP TABLE IF EXISTS EventReport CASCADE;
+DROP TABLE IF EXISTS Ticket CASCADE;
+DROP TABLE IF EXISTS Hashtag CASCADE;
+DROP TABLE IF EXISTS Report CASCADE;
+DROP TABLE IF EXISTS PollOptions CASCADE;
+DROP TABLE IF EXISTS Poll CASCADE;
+DROP TABLE IF EXISTS File CASCADE;
+DROP TABLE IF EXISTS Authenticated CASCADE;
+DROP TABLE IF EXISTS Admin CASCADE;
+DROP TABLE IF EXISTS Event CASCADE;
+DROP TABLE IF EXISTS Users CASCADE;
 DROP TYPE IF EXISTS TypesNotification CASCADE;
+DROP TYPE IF EXISTS TypesMessage CASCADE;
+DROP TYPE IF EXISTS TypesEvent CASCADE;
 
------------------------------------------
--- Drop old default values
------------------------------------------
-
-DROP DEFAULT IF EXISTS DATE;
-DROP DEFAULT IF EXISTS path;
-
------------------------------------------
 -- Create types
------------------------------------------
 
 CREATE TYPE TypesEvent AS ENUM ('public', 'private', 'approval');
 CREATE TYPE TypesMessage AS ENUM ('chat', 'comment');
 CREATE TYPE TypesNotification AS ENUM ('request_answer', 'invitation');
 
------------------------------------------
--- Create Default Values
------------------------------------------
-
-DATE DEFAULT CURRENT_DATE;
-path VARCHAR(255) DEFAULT 'defaultpic.png';
-
------------------------------------------
 -- Create tables
------------------------------------------
 
 CREATE TABLE Users (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
@@ -71,15 +47,15 @@ CREATE TABLE Admin (
 CREATE TABLE Authenticated (
     id_user INT PRIMARY KEY,
     is_verified BOOLEAN DEFAULT FALSE,
-    id_profilepic INT DEFAULT '0',
+    id_profilepic INT DEFAULT 0,
     FOREIGN KEY (id_user) REFERENCES Users(id)
 );
 
 CREATE TABLE Event (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    type VARCHAR(255) CHECK (type IN ('TypesEvent')),
+    type TypesEvent DEFAULT 'public',
     date DATE NOT NULL,
     capacity INT NOT NULL,
     place VARCHAR(255) NOT NULL,
@@ -88,12 +64,12 @@ CREATE TABLE Event (
 );
 
 CREATE TABLE EventMessage (
-    id INT PRIMARY KEY,
-    type VARCHAR(255) CHECK (type IN ('TypesMessage')),
+    id SERIAL PRIMARY KEY,
+    type TypesMessage NOT NULL,
     content TEXT NOT NULL,
     id_event INT NOT NULL,
     id_user INT NOT NULL,
-    date DATE DEFAULT CURRENT_DATE
+    date DATE DEFAULT CURRENT_DATE,
     FOREIGN KEY (id_event) REFERENCES Event(id),
     FOREIGN KEY (id_user) REFERENCES Authenticated(id_user)
 );
@@ -106,8 +82,8 @@ CREATE TABLE MessageReaction (
 );
 
 CREATE TABLE Notification (
-    id INT PRIMARY KEY,
-    type VARCHAR(255) CHECK (type IN ('TypesNotification')),
+    id SERIAL PRIMARY KEY,
+    type TypesNotification NOT NULL,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     date DATE DEFAULT CURRENT_DATE,
@@ -137,26 +113,19 @@ CREATE TABLE FavoriteEvent (
 );
 
 CREATE TABLE Hashtag (
-    id INT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    id_event INT NOT NULL,
-    FOREIGN KEY (id_event) REFERENCES Event(id)
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Hashtag{
- id INT PRIMARY KEY,
- title VARCHAR(255) NOT NULL,
-};
-
-CREATE TABLE EventHashtag {
- id_event INT NOT NULL,
- id_hashtag INT NOT NULL,
- FOREIGN KEY (id_event) REFERENCES Event(id),
- FOREIGN KEY (id_hashtag) REFERENCES Hashtag(id)
-};
+CREATE TABLE EventHashtag (
+    id_event INT NOT NULL,
+    id_hashtag INT NOT NULL,
+    FOREIGN KEY (id_event) REFERENCES Event(id),
+    FOREIGN KEY (id_hashtag) REFERENCES Hashtag(id)
+);
 
 CREATE TABLE Ticket (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     id_event INT NOT NULL,
     id_user INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
@@ -167,7 +136,7 @@ CREATE TABLE Ticket (
 );
 
 CREATE TABLE Report (
-    id INT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL
 );
@@ -179,8 +148,8 @@ CREATE TABLE EventReport (
     FOREIGN KEY (id_report) REFERENCES Report(id)
 );
 
-CREATE TABLE File (
-    id INT PRIMARY KEY,
+CREATE TABLE UploadFile (
+    id SERIAL PRIMARY KEY,
     path VARCHAR(255) NOT NULL,
     id_user INT NOT NULL,
     id_message INT,
@@ -192,27 +161,29 @@ CREATE TABLE File (
     FOREIGN KEY (id_event) REFERENCES Event(id)
 );
 
-CREATE TABLE Poll{
-    id INT PRIMARY KEY,
+CREATE TABLE Poll (
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    summary TEXT NOT NULL,
+    summary TEXT,
     question TEXT NOT NULL,
-    startsAt DATE NOT NULL,
+    startsAt DATE DEFAULT CURRENT_DATE,
     endsAt DATE NOT NULL,
     id_event INT NOT NULL,
-    id user INT NOT NULL, /* Duvidas */
+    id_user INT NOT NULL,
     FOREIGN KEY (id_event) REFERENCES Event(id),
     FOREIGN KEY (id_user) REFERENCES Authenticated(id_user)
-};
+);
 
-CREATE TABLE poll_options{
-    int id INT PRIMARY KEY,
+CREATE TABLE PollOptions (
+    id SERIAL PRIMARY KEY,
     option TEXT NOT NULL,
-    id_pool INT NOT NULL,
+    id_poll INT NOT NULL,
     FOREIGN KEY (id_poll) REFERENCES Poll(id)
-};
+);
 
-CREATE TABLE poll_votes{
- int id_user INT NOT NULL,
- int id_option INT NOT NULL,
-};
+CREATE TABLE PollVotes (
+    id_user INT NOT NULL,
+    id_option INT NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES Authenticated(id_user),
+    FOREIGN KEY (id_option) REFERENCES PollOptions(id)
+);
