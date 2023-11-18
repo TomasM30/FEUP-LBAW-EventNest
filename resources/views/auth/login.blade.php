8b2c09c752c63a44028f5cf1e403c5e1446d91d8
up202105327 <up202105327@fe.up.pt>
2023-11-18 17:38:12 +0000

    Added jscript to show forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
             <div id='left' class='column'>
                 <img src="{{ asset('images/Logo.png') }}" alt="EventNest" class="img-fluid" id='logo'>
                 <p class="text-md" id="catch-frase1">
-                    Unleash the Power of Community: Where Every Passion Finds Its Event. Join, Create, and Thrive with EventNest - Your Gateway to Inclusive Sports Experiences!
+                    Unleash the Power of Community: Where Every Passion Finds Its Event. Join, Create, and Thrive with EventNest. - Your Gateway to Inclusive Sports Experiences!
                 </p>
                 <hr>
                 <p class="text-md" id="information1">
@@ -24,7 +24,34 @@
 
                 <!-- Differentiate buttons using the name attribute -->
                 <button id='login' type="submit" name="action" value="login" class="btn btn-primary btn-block">Login</button>
+                <div id="loginFormContainer" style="display: none;">
+                    <!-- Add your login form fields here -->
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" name="email" required>
+
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" name="password" required>
+
+                    <button id='login-button' type="submit" class="btn btn-primary btn-block">Login</button>
+                </div>
                 <button id='register' type="submit" name="action" value="register" class="btn btn-primary btn-block">Register</button>
+                <div id="registerFormContainer" style="display: none;">
+                    <!-- Add your register form fields here -->
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" name="name" required>
+
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" name="email" required>
+
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" name="password" required>
+
+                    <label for="confirmpassword">Confirm Password:</label>
+                    <input type="password" class="form-control" name="password_confirmation" required>
+
+                    <button id='login-button' type="submit" class="btn btn-primary btn-block">Register</button>
+
+                </div>
                 <button id='gmail' type="submit" name="action" value="gmail" class="btn btn-primary btn-block">
                     <img src="{{ asset('images/Gmail.png') }}" class="img-fluid">
                 </button>
@@ -32,4 +59,26 @@
         </div>
     </div>
 </form>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // Add a click event listener to the "Login" button
+        $('#login').click(function() {
+            // Toggle the visibility of the login form container
+            $('#loginFormContainer').show();
+            $('#login').hide();
+            $('#registerFormContainer').hide();
+            $('#register').show();
+        })
+        // Add a click event listener to the "Register" button
+        $('#register').click(function() {
+            // Toggle the visibility of the register form container
+            $('#registerFormContainer').show();
+            $('#register').hide();
+            $('#loginFormContainer').hide();
+            $('#login').show();
+        })
+    });
+</script>
 @endsection
