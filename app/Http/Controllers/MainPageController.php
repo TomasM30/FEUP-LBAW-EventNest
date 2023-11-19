e3cdcd468cae892366c1812e131d0b3af9ab3551
Tomás Martins <up202108776@fe.up.pt>
2023-11-19 18:17:44 +0000

    updated

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\View\View;
+
+
 class MainPageController extends Controller
 {
     public function showForm() {
-        return view('test');
+        return view('pages.test');
     }
 }
