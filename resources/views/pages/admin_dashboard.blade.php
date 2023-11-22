4d5a0192ad9c8acc714b62d55c0325cb02f5f23a
up202105327 <up202105327@fe.up.pt>
2023-11-22 21:16:12 +0000

    Added new CSS to main page and Dashboard

--- resources/views/pages/admin_dashboard.blade.php
+++ resources/views/pages/admin_dashboard.blade.php
@@ -1,20 +1,42 @@
 @extends('layouts.app')
 
-
 @section('content')
-    <h1>Admin Dashboard</h1>
+<h1 class="dashboardtitle">Admin Dashboard</h1>
+<div class="dashboard-section">
+    <h2 class="tabletitle">Users</h2>
+    <table class="user-table table">
+        <thead>
+            <tr>
+                <th>Username</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $authenticatedUser)
+            <tr>
+                <td><a href="{{ url('/user/' . $authenticatedUser->user->id . '/events') }}">{{ $authenticatedUser->user->username }}</a></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
-    <h2>Users</h2>
-    <ul>
-        @foreach ($users as $authenticatedUser)
-            <li><a href="{{ url('/user/' . $authenticatedUser->user->id . '/events') }}">{{ $authenticatedUser->user->username }}</a></li>
-        @endforeach
-    </ul>
-
-    <h2>Events</h2>
-    <ul>
-        @foreach ($events as $event)
-            <li>{{ $event->title }} - {{ $event->date }}</li>
-        @endforeach
-    </ul>
+<div class="dashboard-section">
+    <h2 class="tabletitle">Events</h2>
+    <table class="event-table table">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($events as $event)
+            <tr>
+                <td>{{ $event->title }}</td>
+                <td>{{ $event->date }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
--- resources/views/pages/user_events.blade.php
+++ resources/views/pages/user_events.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>{{ $user->name }}'s Events</h1>
+<h1 class="usertitle">{{ $user->name }}'s Events</h1>
 
-<div id="content">
+<div class="myevents">
     <div id="created">
         <h2>Created Events</h2>
         @foreach ($createdEvents as $event)
-            @include('partials.event', ['event' => $event])
+        @include('partials.event', ['event' => $event])
         @endforeach
     </div>
     <div id="joined">
         <h2>Joined Events</h2>
         @foreach ($joinedEvents as $event)
-            @include('partials.event', ['event' => $event])
+        @include('partials.event', ['event' => $event])
         @endforeach
     </div>
     <div id="favorite">
         <h2>Favorite Events</h2>
         @foreach ($favoriteEvents as $event)
-            @include('partials.event', ['event' => $event])
+        @include('partials.event', ['event' => $event])
         @endforeach
     </div>
 </div>
