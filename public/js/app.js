48a782a4f8462bfff775effc96310ed9f12cb01b
Tomás Martins <up202108776@fe.up.pt>
2023-12-01 15:12:43 +0000

    added corrected css

--- public/js/app.js
+++ public/js/app.js
@@ -268,7 +268,7 @@ document.addEventListener('DOMContentLoaded', function() {
           xhr.open('GET', url + '?search=' + value, true);
           xhr.onreadystatechange = function() {
               if (xhr.readyState == 4 && xhr.status == 200) {
-                  document.getElementById('main').innerHTML = xhr.responseText;
+                  document.getElementById('container').innerHTML = xhr.responseText;
               }
           }
           xhr.send();
